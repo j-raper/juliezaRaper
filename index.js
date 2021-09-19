b956264b64e620f9df2cecaf4993237c3ea99505
j-raper <jraper_200000000052@uic.edu.ph>
2021-09-19 23:24:03 +0800

    finalizing digital portfolio

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const from = document.getElementById('form');
-const from = document.getElementById('firstname');
-const from = document.getElementById('lastname');
-const from = document.getElementById('suffix');
-const from = document.getElementById('email');
-const from = document.getElementById('gender');
-const from = document.getElementById('contact');
-const from = document.getElementById('chooseWebsite');
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const suffix = document.getElementById('suffix');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const choosewebsite = document.getElementById('choosewebsite');
 
-from.addEventListener('submit', (e) => {
-        e.preventDefault();
-        checkInputs();
+console.log('hello')
+
+form.addEventListener('submit', (e) => {
+    console.log('hey u here')
+    e.preventDefault();
+    checkInputs();
 });
 
 function checkInputs() {
@@ -17,28 +19,62 @@ function checkInputs() {
     const lastnameValue = lastname.value.trim();
     const suffixValue = suffix.value.trim();
     const emailValue = email.value.trim();
-    const genderValue = gender.value.trim();
-    const contactValue = contact.value.trim();
-    const chooseWebsiteValue = chooseWebsite.value.trim();
+    const contactValue = phone.value.trim();
+    const chooseWebsiteValue = choosewebsite.value.trim();
 
-if(firstnameValue === '') {
-    //show error
-    setErrorFor(firstname, 'Firstname cabbot be blank')
-}else{
-    //add success
-    setSuccessFor(firstname);
+    if(firstnameValue === '') {
+        //show error
+        setErrorFor(firstname, 'Firstname cannot be blank')
+    }else{
+        //add success
+        setSuccessFor(firstname);
     }
+
+    if(lastnameValue === '') {
+        //show error
+        setErrorFor(lastname, 'Firstname cannot be blank')
+    }else{
+        //add success
+        setSuccessFor(lastname);
+    }
+
+    if(suffixValue === '') {
+        //show error
+        setErrorFor(suffix, 'Firstname cannot be blank')
+    }else{
+        //add success
+        setSuccessFor(suffix);
+    }
+
+    if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+    
+    if(contactValue === '') {
+        //show error
+        setErrorFor(phone, 'Firstname cannot be blank')
+    }else{
+        //add success
+        setSuccessFor(phone);
+    }    
 }
 
 function setErrorFor(input, message) {
-    const personalInformation = input.parentElement; //divpersonalinfo
-    const small = personalInformation.querySelector('small');
+    const personalinformation = input.parentElement; //divpersonalinfo
+    const small = personalinformation.querySelector('small');
 
     small.innerText = message;
-    personalInformation.className = 'personalInformation error';
+    personalinformation.className = 'personalInformation error';
 }
 function setSuccessFor(input) {
     const personalInformation = input.parentElement;
     personalInformation.className = 'personalInformation success'
+}
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
  
